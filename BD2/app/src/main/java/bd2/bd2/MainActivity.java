9744048f21f51defcd015ca0b4e5b12bd15fdef0
unknown <alex_pili@hotmail.it>
2016-06-18 10:03:15 +0200

    setup and first query example

---
+++ BD2/app/src/main/java/bd2/bd2/MainActivity.java
@@ -0,0 +1,29 @@
+package bd2.bd2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+
+public class MainActivity extends Activity{
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.listView = (ListView) findViewById(R.id.listView);
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+        databaseAccess.open();
+        List<String> quotes = databaseAccess.firstQuery();
+        databaseAccess.close();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, quotes);
+        this.listView.setAdapter(adapter);
+    }
+}
