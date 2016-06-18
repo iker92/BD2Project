2e51544036663b4e955f52e9e4f355131d894395
unknown <alex_pili@hotmail.it>
2016-06-18 11:57:33 +0200

    fixed dbType, done first simple query based on spatialite

--- BD2/app/src/main/java/bd2/bd2/DatabaseAccess.java
+++ BD2/app/src/main/java/bd2/bd2/DatabaseAccess.java
@@ -5,26 +5,80 @@ package bd2.bd2;
  */
 
         import android.content.Context;
+        import android.content.res.AssetManager;
         import android.database.Cursor;
-        import android.database.sqlite.SQLiteDatabase;
-        import android.database.sqlite.SQLiteOpenHelper;
+        import jsqlite.*;
+        import jsqlite.Exception;
 
+        import android.database.sqlite.SQLiteOpenHelper;
+        import android.util.Log;
+
+        import java.io.File;
+        import java.io.FileOutputStream;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.OutputStream;
         import java.util.ArrayList;
         import java.util.List;
 
 public class DatabaseAccess {
+
+    private static final String TAG ="GEODBH" ;
+    private static final String TAG_SL = TAG + "_JSQLITE" ;
     private SQLiteOpenHelper openHelper;
-    private SQLiteDatabase database;
+    private Database database;
     private static DatabaseAccess instance;
+    private static String DB_NAME = "dbProva.sqlite";
+    private static String DB_PATH = "/data/data/bd2.bd2/databases";
 
     /**
      * Private constructor to aboid object creation from outside classes.
      *
      * @param context
      */
-    private DatabaseAccess(Context context) {
-        this.openHelper = new DatabaseOpenHelper(context);
+    private DatabaseAccess(Context context) throws IOException {
+        /*this.openHelper = new DatabaseOpenHelper(context);
+        database = new Database();*/
+        File cacheDatabase = new File(DB_PATH, DB_NAME);
+        if (!cacheDatabase.getParentFile().exists()) {
+            File dirDb = cacheDatabase.getParentFile();
+            Log.i(TAG,"making directory: " + cacheDatabase.getParentFile());
+            if (!dirDb.mkdir()) {
+                throw new IOException(TAG_SL + "Could not create dirDb: " + dirDb.getAbsolutePath());
+            }
+        }
+
+
+        InputStream inputStream = context.getAssets().open("databases/dbProva.sqlite");
+        copyDatabase(inputStream, DB_PATH + File.separator + DB_NAME);
+        database= new Database();
+        
+        try {
+            database.open(cacheDatabase.getAbsolutePath(),
+                    jsqlite.Constants.SQLITE_OPEN_READWRITE | jsqlite.Constants.SQLITE_OPEN_CREATE);
+        } catch (jsqlite.Exception e) {
+            Log.e(TAG_SL,e.getMessage());
+        }
+
+
     }
+
+    private void copyDatabase(InputStream inputStream, String dbFilename) throws IOException {
+        OutputStream outputStream = new FileOutputStream(dbFilename);
+
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inputStream.read(buffer)) > 0) {
+            outputStream.write(buffer,0,length);
+        }
+
+        outputStream.flush();
+        outputStream.close();
+        inputStream.close();
+        Log.i(TAG,"Copied database to " + dbFilename);
+    }
+
+
 
     /**
      * Return a singleton instance of DatabaseAccess.
@@ -32,24 +86,22 @@ public class DatabaseAccess {
      * @param context the Context
      * @return the instance of DabaseAccess
      */
-    public static DatabaseAccess getInstance(Context context) {
+    public static DatabaseAccess getInstance(Context context) throws IOException {
         if (instance == null) {
             instance = new DatabaseAccess(context);
         }
         return instance;
     }
 
-    /**
-     * Open the database connection.
-     */
-    public void open() {
-        this.database = openHelper.getWritableDatabase();
-    }
+
+
+   
+
 
     /**
      * Close the database connection.
      */
-    public void close() {
+    public void close() throws Exception {
         if (database != null) {
             this.database.close();
         }
@@ -60,7 +112,34 @@ public class DatabaseAccess {
      *
      * @return a List of quotes
      */
-    public List<String> firstQuery() {
+
+    public String queryTableSimple() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("query comuni table..");
+
+        String query = "select NOME from DBTComune order by NOME";
+        stringBuilder.append("Execute query: ").append(query).append("\n");
+
+        try {
+            Stmt stmt = database.prepare(query);
+            int index = 0;
+            while (stmt.step()) {
+                String result = stmt.column_string(0);
+                stringBuilder.append("\t").append(result).append("\n");
+                if (index++ > 10) break;
+            }
+            stringBuilder.append("\t...");
+            stmt.close();
+        } catch (jsqlite.Exception e) {
+            Log.e(TAG_SL,e.getMessage());
+        }
+
+        stringBuilder.append("done\n");
+
+        return stringBuilder.toString();
+    }
+
+  /*  public List<String> firstQuery() {
         List<String> list = new ArrayList<>();
         Cursor cursor = database.rawQuery("SELECT NOME FROM DBTComune WHERE NOME= 'DECIMOPUTZU'", null);
         cursor.moveToFirst();
@@ -70,5 +149,5 @@ public class DatabaseAccess {
         }
         cursor.close();
         return list;
-    }
+    }*/
 }
--- BD2/app/src/main/java/bd2/bd2/MainActivity.java
+++ BD2/app/src/main/java/bd2/bd2/MainActivity.java
@@ -2,10 +2,17 @@ package bd2.bd2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import jsqlite.Exception;
 
 
 public class MainActivity extends Activity{
@@ -17,14 +24,24 @@ public class MainActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.listView = (ListView) findViewById(R.id.listView);
-        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
-        databaseAccess.open();
-        List<String> quotes = databaseAccess.firstQuery();
-        databaseAccess.close();
+        TextView result=(TextView) findViewById(R.id.result);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, quotes);
-        this.listView.setAdapter(adapter);
+        this.listView = (ListView) findViewById(R.id.listView);
+        DatabaseAccess databaseAccess = null;
+        try {
+            databaseAccess = DatabaseAccess.getInstance(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String quotes=databaseAccess.queryTableSimple();
+        try {
+            databaseAccess.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+      result.setText(quotes);
     }
 }
 //crilly
