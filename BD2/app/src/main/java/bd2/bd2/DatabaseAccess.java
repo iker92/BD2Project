dc1c8aac99ea8de029dea039e5b3e72a1d0acae5
crilly <crilly@github.com>
2016-06-21 18:40:56 +0200

    seconda versione

--- BD2/app/src/main/java/bd2/bd2/DatabaseAccess.java
+++ BD2/app/src/main/java/bd2/bd2/DatabaseAccess.java
@@ -11,7 +11,18 @@ package bd2.bd2;
         import jsqlite.Exception;
 
         import android.database.sqlite.SQLiteOpenHelper;
+        import android.graphics.Color;
         import android.util.Log;
+
+        import com.esri.android.map.GraphicsLayer;
+        import com.esri.core.geometry.Geometry;
+        import com.esri.core.geometry.GeometryEngine;
+        import com.esri.core.geometry.Point;
+        import com.esri.core.geometry.Polygon;
+        import com.esri.core.geometry.SpatialReference;
+        import com.esri.core.map.Graphic;
+        import com.esri.core.symbol.SimpleMarkerSymbol;
+        import com.esri.core.symbol.Symbol;
 
         import java.io.File;
         import java.io.FileOutputStream;
@@ -25,11 +36,15 @@ public class DatabaseAccess {
 
     private static final String TAG ="GEODBH" ;
     private static final String TAG_SL = TAG + "_JSQLITE" ;
+    private static final boolean ERROR =true ;
     private SQLiteOpenHelper openHelper;
     private Database database;
+    Graphic [] graphics1;
+    ArrayList<Point> point_result=new ArrayList<>();
     private static DatabaseAccess instance;
     private static String DB_NAME = "dbProva.sqlite";
     private static String DB_PATH = "/data/data/bd2.bd2/databases";
+
 
     /**
      * Private constructor to aboid object creation from outside classes.
@@ -137,6 +152,116 @@ public class DatabaseAccess {
         stringBuilder.append("done\n");
 
         return stringBuilder.toString();
+    }
+
+    public void changeReference() throws Exception {
+
+        String query="ALTER TABLE "+ database.getFilename()+".DBTComune " +
+                "ALTER COLUMN Geometry TYPE geometry(MULTIPOLYGON, 3003) USING ST_Transform(Geometry, 3003);";
+ Stmt stmt=database.prepare(query);
+
+    }
+
+    public ArrayList<Point> queryComuniNearby() {
+
+
+
+        StringBuilder sb = new StringBuilder();
+        //sb.append(SEP);
+        sb.append("Query Comuni nearby...\n");
+
+        String query = "SELECT Hex(ST_AsBinary(ST_Buffer(Geometry, 1.0))), ST_Srid(Geometry), ST_GeometryType(Geometry) from DBTComune" +
+                " where NOME = 'DECIMOPUTZU';";
+        sb.append("Execute query: ").append(query).append("\n");
+        String bufferGeom = "";
+        String bufferGeomShort = "";
+        try {
+            Stmt stmt = database.prepare(query);
+            if (stmt.step()) {
+                bufferGeom = stmt.column_string(0);
+                String geomSrid = stmt.column_string(1);
+                String geomType = stmt.column_string(2);
+
+                sb.append("\tThe selected geometry is of type: ").append(geomType).append(" and of SRID: ").append(geomSrid).append("\n");
+            }
+            System.out.println(sb);
+            bufferGeomShort = bufferGeom;
+            if (bufferGeom.length() > 10)
+                bufferGeomShort = bufferGeom.substring(0, 10) + "...";
+            sb.append("\tDecimoputzu polygon buffer geometry in HEX: ").append(bufferGeomShort).append("\n");
+            stmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            sb.append(ERROR).append(e.getLocalizedMessage()).append("\n");
+        }
+/*  String query = "select * from districts where within("
+        + "ST_Transform(GeomFromText('"
+                + gpsPoint + "', " + GPS_SRID
+                + "), " + SOURCE_DATA_SRID + "),districts.Geometry);";*/
+        query = "SELECT  NOME , ASText(ST_centroid(Geometry)) from DBTComune where ST_Intersects( ST_GeomFromWKB(x'" + bufferGeom + "') ,Geometry);";
+        // just for print
+        String tmpQuery = "SELECT NOME from DBTComune where ST_Intersects( ST_GeomFromWKB(x'" + bufferGeomShort + "') , Geometry );";
+        sb.append("Execute query: ").append(tmpQuery).append("\n");
+        try {
+            sb.append("\tComuni nearby Decimoputzu: \n");
+            Stmt stmt = database.prepare(query);
+
+            ArrayList<Graphic> graphics = new ArrayList<>();
+            while( stmt.step() ) {
+                String name = stmt.column_string(0);
+                String wkt = stmt.column_string(1);
+
+                // mettere i punti trovati in un array per poi creare la polyline associata
+
+               double x = Double.valueOf(wkt.substring(6,20));
+                double y = Double.valueOf(wkt.substring(20,34));
+                String query_points = "SELECT AsText(Transform(MakePoint(" + x + ", " + y + ", 3003), 4326));";
+                database.prepare(query_points);
+                double x_2 = 0.0;
+                double y_2 = 0.0;
+                try {
+                    Stmt stmt_2 = database.prepare(query_points);
+                    while (stmt_2.step()) {
+                        String pointStr = stmt_2.column_string(0);
+                        String first = (String) pointStr.substring(6);
+                        String[] split = first.split(" ");
+                        first = split[0];
+                        String second = split[1];
+                        if(second.endsWith(")"))
+                        {
+                            second = second.substring(0,second.length()-1);
+                        }
+
+                        x_2 = Double.parseDouble(first);
+                        y_2 = Double.parseDouble(second);
+
+                        Point point = new Point(x_2, y_2);
+                        point_result.add(point);
+
+                        System.out.println("X: " + point.getX() + " Y: " + point.getY());
+                        sb.append("\t\t").append(name).append(" - with centroid in ").append(wkt).append("\n");
+                    }
+                    stmt_2.close();
+                }catch (Exception e) {
+                    e.printStackTrace();
+                    sb.append(ERROR).append(e.getLocalizedMessage()).append("\n");
+                }
+            }
+            /*graphics1=new Graphic[graphics.size()];
+            for (int i = 0; i <graphics1.length ; i++) {
+                graphics1[i]=graphics.get(i);
+            }*/
+            stmt.close();
+
+            //layer.addGraphics(graphics1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            sb.append(ERROR).append(e.getLocalizedMessage()).append("\n");
+        }
+        sb.append("Done...\n");
+
+        return point_result;
     }
 
   /*  public List<String> firstQuery() {
--- BD2/app/src/main/java/bd2/bd2/MainActivity.java
+++ BD2/app/src/main/java/bd2/bd2/MainActivity.java
@@ -1,12 +1,28 @@
 package bd2.bd2;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+
+//import com.esri.arcgisruntime.*;
+//import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.android.map.GraphicsLayer;
+import com.esri.android.map.MapOptions;
+import com.esri.android.map.MapView;
+import com.esri.android.map.ags.ArcGISTiledMapServiceLayer;
+import com.esri.android.map.event.OnStatusChangedListener;
+import com.esri.core.geometry.Envelope;
+import com.esri.core.geometry.GeometryEngine;
+import com.esri.core.geometry.Point;
+import com.esri.core.geometry.SpatialReference;
+import com.esri.core.map.Graphic;
+import com.esri.core.symbol.SimpleMarkerSymbol;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,15 +34,18 @@ import jsqlite.Exception;
 public class MainActivity extends Activity{
 
     private ListView listView;
+    MapView mMapView;
+    boolean flag=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMapView = (MapView) findViewById(R.id.map);
+        //TextView result=(TextView) findViewById(R.id.result);
 
-        TextView result=(TextView) findViewById(R.id.result);
 
-        this.listView = (ListView) findViewById(R.id.listView);
+        //this.listView = (ListView) findViewById(R.id.listView);
         DatabaseAccess databaseAccess = null;
         try {
             databaseAccess = DatabaseAccess.getInstance(this);
@@ -34,14 +53,39 @@ public class MainActivity extends Activity{
             e.printStackTrace();
         }
 
-        String quotes=databaseAccess.queryTableSimple();
+        //  GraphicsLayer quotes=databaseAccess.queryTableSimple();
+        ArrayList<Point> points = databaseAccess.queryComuniNearby();
+
+        SimpleMarkerSymbol sms = new SimpleMarkerSymbol(Color.RED , 8, SimpleMarkerSymbol.STYLE.CIRCLE);
+        GraphicsLayer layer = new GraphicsLayer();
+        SpatialReference webSR = SpatialReference.create(3857);
+
+        //aggiungo i punti al layer
+        for (int i = 0; i < points.size(); i++) {
+
+            Point webPoint = GeometryEngine.project(points.get(i).getX(), points.get(i).getY(), webSR);
+            layer.addGraphic(new Graphic(webPoint, sms));
+        }
+
+
+        //layer.setVisible(true);
+        //flag=layer.isVisible();
+       /* SpatialReference mSR = graphics[0].getSpatialReference();
+        Point p1 = GeometryEngine.project(39.2305400, 0.0, mSR);
+        Point p2 = GeometryEngine.project(-60.0, 50.0, mSR);
+        Envelope mInitExtent = new Envelope(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+        mMapView.setExtent(mInitExtent);*/
+        //mMapView.addLayer(basemapTileLayer);
+        //mMapView.setEnabled(true);
+        mMapView.addLayer(layer);
+
         try {
             databaseAccess.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-      result.setText(quotes);
+        //Log.d("layer info: ",info);
+        Log.d("layer visibile?",String.valueOf(flag));
+      //result.setText(quotes);
     }
 }
-//crilly
